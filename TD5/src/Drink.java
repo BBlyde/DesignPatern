6094090ef7f3bcadcdba1980b12610c35c59c546
BBlyde <benjaminblondey@orange.fr>
2022-10-04 17:07:47 +0200

    td5 part 1

--- TD5/src/Drink.java
+++ TD5/src/Drink.java
@@ -1,15 +1,17 @@
 public class Drink extends Product{
 
-    private DrinkType name;
-    private double price;
+    public static final Drink Expresso = new Drink("Expresso", 2.75);
+    public static final Drink Chocolat = new Drink("Chocolat", 3.55);
+    public static final Drink Café = new Drink("Café", 1.75);
+    public static final Drink Décaféiné = new Drink("Décaféiné", 2.5);
+    public static final Drink Thé = new Drink("Thé", 3.15);
 
-    public enum DrinkType{
-        Expresso, Chocolat, Café, Décaféiné, Thé
+    public Drink(String name, double price){
+        super(name, price);
     }
 
     @Override
     public double getPrice() {
-    
+        return(price);
     }
-    
 }
