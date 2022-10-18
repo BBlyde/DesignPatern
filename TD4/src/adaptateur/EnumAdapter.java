4ece6dc8c3b9939b89152179eb497665faba34e7
BBlyde <benjaminblondey@orange.fr>
2022-10-18 13:55:08 +0200

    adapter + provider

---
+++ TD4/src/adaptateur/EnumAdapter.java
@@ -0,0 +1,27 @@
+package adaptateur;
+
+import java.util.*;
+
+public class EnumAdapter<E> implements Iterator<E> {
+    
+    private Enumeration<E> e;
+
+    public EnumAdapter(Enumeration<E> e){
+        this.e = e;
+    }
+
+    @Override
+    public boolean hasNext(){
+        return e.hasMoreElements();
+    }
+
+    @Override
+    public E next(){
+        return e.nextElement();
+    }
+
+    @Override
+    public void remove(){
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ TD4/src/procuration/Provider.java
@@ -0,0 +1,8 @@
+package procuration;
+
+public interface Provider {
+    void service1();
+    void service2();
+    void service3();
+    void service4();
+}
